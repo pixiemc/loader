a7c2b61b17875c52a38922ffd66d844ec9cb7fa0
lilyvxv <123564796+lilyvxv@users.noreply.github.com>
2025-01-22 20:28:42 +0000

    meow

---
+++ forge/src/main/java/rip/pixie/forge/Entrypoint.java
@@ -0,0 +1,10 @@
+package rip.pixie.forge;
+
+import net.minecraftforge.fml.common.Mod;
+import rip.pixie.EssentialTransformer;
+
+@Mod(Entrypoint.MODID)
+public class Entrypoint {
+    public static final String MODID = "pixie_loader";
+    // This is required for forge to recognize the mod
+}
